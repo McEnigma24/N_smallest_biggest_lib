3f8f127d889f3f64ef12f819b16b16f96639bedc
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-03-13 18:28:59 +0100

    quick

---
+++ _inc/N_smallest_or_biggest_values.hpp
@@ -0,0 +1,164 @@
+#pragma once
+#include "__preprocessor__.h"
+
+namespace N_smallest_or_biggest_values
+{
+    template <typename value_t, class obj_t>
+    struct entry_t
+    {
+        value_t value;
+        obj_t* obj_ptr;
+
+        entry_t() { memset(this, 0, sizeof(*this)); }
+    };
+
+    template <int tab_size, typename value_t, class obj_t>
+    class N_smallest_values
+    {
+        entry_t<value_t, obj_t> tab[tab_size];
+        int index_of_biggest_value = -1;
+
+    private:
+        void puch_back(const value_t& value, obj_t* obj)
+        {
+            index_of_biggest_value++;
+
+            tab[index_of_biggest_value].value = value;
+            tab[index_of_biggest_value].obj_ptr = obj;
+        }
+
+        void insert(const int index, const value_t& value, obj_t* obj)
+        {
+            if (index_of_biggest_value != (tab_size - 1)) { index_of_biggest_value++; }
+
+            // moving obj to make space //
+            for (int i = index_of_biggest_value; i >= (index + 1); i--)
+            {
+                tab[i] = tab[i - 1];
+            }
+
+            tab[index].value = value;
+            tab[index].obj_ptr = obj;
+        }
+
+    public:
+        void push(const value_t& value, const obj_t* obj)
+        {
+            // first obj //
+            if (index_of_biggest_value == -1)
+            {
+                puch_back(value, (obj_t*)obj);
+                return;
+            }
+
+            // pierwsza wartość jest najmniejsza
+            const value_t smallest_value =
+                tab[0].value; // optymalizacja żeby wyszukiwać binarnie, bo jak będę wkładał we właściwe miejsca to będzie posortowane
+            const value_t biggest_value = tab[index_of_biggest_value].value;
+
+            // jak jest większa od aktualnie największej to dajemy ją na koniec i nic nie przenosimy
+            if (biggest_value < value)
+            {
+                if (index_of_biggest_value == (tab_size - 1))
+                {
+                    // wszystkie pozycje są już zajęte i można odrzucać
+                    return;
+                }
+
+                puch_back(value, (obj_t*)obj);
+                return;
+            }
+
+            // jest gdzieś pomiędzy - trzeba przejść i ustawić na właściwe miejsce
+            for (int i = 0; i < tab_size; i++)
+            {
+                if (value < tab[i].value)
+                {
+                    insert(i, value, (obj_t*)obj);
+                    return;
+                }
+            }
+        }
+
+        entry_t<value_t, obj_t>* get_data(int& size)
+        {
+            size = index_of_biggest_value + 1;
+            return tab;
+        }
+    };
+
+    template <int tab_size, typename value_t, class obj_t>
+    class N_biggest_values
+    {
+        entry_t<value_t, obj_t> tab[tab_size];
+        int index_of_biggest_value = -1;
+
+    private:
+        void puch_back(const value_t& value, obj_t* obj)
+        {
+            index_of_biggest_value++;
+
+            tab[index_of_biggest_value].value = value;
+            tab[index_of_biggest_value].obj_ptr = obj;
+        }
+
+        void insert(const int index, const value_t& value, obj_t* obj)
+        {
+            if (index_of_biggest_value != (tab_size - 1)) { index_of_biggest_value++; }
+
+            // moving obj to make space //
+            for (int i = index_of_biggest_value; i >= (index + 1); i--)
+            {
+                tab[i] = tab[i - 1];
+            }
+
+            tab[index].value = value;
+            tab[index].obj_ptr = obj;
+        }
+
+    public:
+        void push(const value_t& value, const obj_t* obj)
+        {
+            // first obj //
+            if (index_of_biggest_value == -1)
+            {
+                puch_back(value, (obj_t*)obj);
+                return;
+            }
+
+            // pierwsza wartość jest najmniejsza
+            const value_t smallest_value =
+                tab[0].value; // optymalizacja żeby wyszukiwać binarnie, bo jak będę wkładał we właściwe miejsca to będzie posortowane
+            const value_t biggest_value = tab[index_of_biggest_value].value;
+
+            // jak jest większa od aktualnie największej to dajemy ją na koniec i nic nie przenosimy
+            if (biggest_value < value)
+            {
+                if (index_of_biggest_value == (tab_size - 1))
+                {
+                    // wszystkie pozycje są już zajęte i można odrzucać
+                    return;
+                }
+
+                puch_back(value, (obj_t*)obj);
+                return;
+            }
+
+            // jest gdzieś pomiędzy - trzeba przejść i ustawić na właściwe miejsce
+            for (int i = 0; i < tab_size; i++)
+            {
+                if (value < tab[i].value)
+                {
+                    insert(i, value, (obj_t*)obj);
+                    return;
+                }
+            }
+        }
+
+        entry_t<value_t, obj_t>* get_data(int& size)
+        {
+            size = index_of_biggest_value + 1;
+            return tab;
+        }
+    };
+} // namespace N_smallest_or_biggest_values
--- _test/test.cpp
+++ _test/test.cpp
@@ -1,10 +1,107 @@
 // clang-format off
 #include <gtest/gtest.h> // must be first
-#include "__preprocessor__.h"
+// clang-format on
+#include "N_smallest_or_biggest_values.hpp"
 
-TEST(SampleTest, Sanity)
+struct OBJ
 {
-    EXPECT_EQ(1, 1);
+    int a;
+    int b;
+    int c;
+
+    OBJ(int _a = 0, int _b = 0, int _c = 0) : a(_a), b(_b), c(_c) {}
+};
+
+TEST(SampleTest, smallest_3)
+{
+    N_smallest_or_biggest_values::N_smallest_values<3, int, OBJ> smallest_values;
+    int value;
+
+    value = 3;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = 5;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = 2;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = 1;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = 4;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = 10;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = 20;
+    smallest_values.push(value, new OBJ(value, value, value));
+
+    int size{};
+    N_smallest_or_biggest_values::entry_t<int, OBJ>* tab = smallest_values.get_data(size);
+
+    EXPECT_EQ(size, 3);
+
+    EXPECT_EQ(tab[base_0(1)].value, 1);
+    EXPECT_EQ(tab[base_0(1)].obj_ptr->a, 1);
+    EXPECT_EQ(tab[base_0(1)].obj_ptr->b, 1);
+    EXPECT_EQ(tab[base_0(1)].obj_ptr->c, 1);
+
+    EXPECT_EQ(tab[base_0(2)].value, 2);
+    EXPECT_EQ(tab[base_0(2)].obj_ptr->a, 2);
+    EXPECT_EQ(tab[base_0(2)].obj_ptr->b, 2);
+    EXPECT_EQ(tab[base_0(2)].obj_ptr->c, 2);
+
+    EXPECT_EQ(tab[base_0(3)].value, 3);
+    EXPECT_EQ(tab[base_0(3)].obj_ptr->a, 3);
+    EXPECT_EQ(tab[base_0(3)].obj_ptr->b, 3);
+    EXPECT_EQ(tab[base_0(3)].obj_ptr->c, 3);
+}
+
+TEST(SampleTest, smallest_5)
+{
+    N_smallest_or_biggest_values::N_smallest_values<5, int, OBJ> smallest_values;
+    int value;
+
+    value = 16468;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = -56559;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = 4541;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = 5;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = 0;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = 100;
+    smallest_values.push(value, new OBJ(value, value, value));
+    value = -2000;
+    smallest_values.push(value, new OBJ(value, value, value));
+
+    int size{};
+    N_smallest_or_biggest_values::entry_t<int, OBJ>* tab = smallest_values.get_data(size);
+
+    EXPECT_EQ(size, 5);
+
+    EXPECT_EQ(tab[base_0(1)].value, -56559);
+    EXPECT_EQ(tab[base_0(1)].obj_ptr->a, -56559);
+    EXPECT_EQ(tab[base_0(1)].obj_ptr->b, -56559);
+    EXPECT_EQ(tab[base_0(1)].obj_ptr->c, -56559);
+
+    EXPECT_EQ(tab[base_0(2)].value, -2000);
+    EXPECT_EQ(tab[base_0(2)].obj_ptr->a, -2000);
+    EXPECT_EQ(tab[base_0(2)].obj_ptr->b, -2000);
+    EXPECT_EQ(tab[base_0(2)].obj_ptr->c, -2000);
+
+    EXPECT_EQ(tab[base_0(3)].value, 0);
+    EXPECT_EQ(tab[base_0(3)].obj_ptr->a, 0);
+    EXPECT_EQ(tab[base_0(3)].obj_ptr->b, 0);
+    EXPECT_EQ(tab[base_0(3)].obj_ptr->c, 0);
+
+    EXPECT_EQ(tab[base_0(4)].value, 5);
+    EXPECT_EQ(tab[base_0(4)].obj_ptr->a, 5);
+    EXPECT_EQ(tab[base_0(4)].obj_ptr->b, 5);
+    EXPECT_EQ(tab[base_0(4)].obj_ptr->c, 5);
+
+    EXPECT_EQ(tab[base_0(5)].value, 100);
+    EXPECT_EQ(tab[base_0(5)].obj_ptr->a, 100);
+    EXPECT_EQ(tab[base_0(5)].obj_ptr->b, 100);
+    EXPECT_EQ(tab[base_0(5)].obj_ptr->c, 100);
 }
 
 int main(int argc, char** argv)
